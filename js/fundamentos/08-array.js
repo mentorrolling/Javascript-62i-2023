64acea5a654cb6442f0a4c4bb7f0b4cb22161038
pmarino2013 <pmarino2013@gmail.com>
2023-07-12 17:34:43 -0300

    Funciones 1era parte

--- js/fundamentos/08-array.js
+++ js/fundamentos/08-array.js
@@ -78,3 +78,42 @@ if (!isNaN(numero)) {
 } else {
   console.warn("Debes ingresar un número");
 }
+
+//Ejercicio
+//----------
+// Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+// Mostrar la longitud del arreglo.
+// Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+// Añade en última posición la ciudad de París.
+// Escribe por pantalla el elemento que ocupa la segunda posición.
+// Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+
+let ciudad = prompt("Ingrese una ciudad");
+let ciudades = [];
+
+//bucle
+//while, do while, for
+while (ciudad) {
+  ciudades.push(ciudad);
+  ciudad = prompt("Ingrese una ciudad");
+}
+
+console.log(`La cantidad de ciudades agregadas es ${ciudades.length}`);
+
+// Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+// document.write(ciudades[0],ciudades[2],ciudades.at(-1))
+document.write("<ul>");
+document.write(`<li>${ciudades[0]}</li>`);
+document.write(`<li>${ciudades[2]}</li>`);
+document.write(`<li>${ciudades.at(-1)}</li>`);
+document.write("</ul>");
+
+// Añade en última posición la ciudad de París.
+ciudades.push("París");
+
+// Escribe por pantalla el elemento que ocupa la segunda posición.
+document.write(`<p>${ciudades[1]}</p>`);
+
+// Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+ciudades.splice(1, 1, "Barcelona");
